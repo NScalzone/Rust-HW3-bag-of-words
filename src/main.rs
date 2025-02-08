b91df64c34f42fa42bd5d53f267fdc1f2cbc66b7
scalzone <scalzone@pdx.edu>
2025-02-08 12:14:20 -0800

    saving progress

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use bbow::Bbow;
+
+fn main() {
+    // println!("Hello")
+    let mut my_bag = Bbow::new();
+    my_bag = my_bag.extend_from_text("This is a te'st string");
+    for key in my_bag.words(){
+        println!("Key: {}", key);   
+    }
+    // let is_count = my_bag.count("is");
+    // println!("'is' is in the list {} times", is_count)
+    if my_bag.is_empty() {
+        println!("bag is empty");
+    }
+    else{
+        println!("bag is not empty");
+    }
+}
